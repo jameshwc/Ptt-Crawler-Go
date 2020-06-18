44a0725c151df44cab227f9ab822dc76656c2613
jameshwc <b07902001@csie.ntu.edu.tw>
2020-06-18 17:43:30 +0800

    finish CrawlArticle

--- ptt.go
+++ ptt.go
@@ -1,21 +1,49 @@
 package ptt
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	. "strings"
 
+	"github.com/PuerkitoBio/goquery"
 	mapset "github.com/deckarep/golang-set"
 )
 
+// crawlBoard
+//
+
 type PTT struct {
-	baseUrl string
-	board   mapset.Set
+	baseUrl   string
+	storePath string
+	board     mapset.Set
 }
 
-func NewPTT() *PTT {
+type reply struct {
+	Floor     int    `json:"floor"`
+	UserID    string `json:"author"`
+	Vote      string `json:"vote"`
+	Content   string `json:"content"`
+	Timestamp string `json:"timestamp"`
+}
+
+type article struct {
+	Board     string  `json:"Board"`
+	Class     string  `json:"Class"`
+	Title     string  `json:"Title"`
+	Author    string  `json:"Author"`
+	Timestamp string  `json:"Timestamp"`
+	Content   string  `json:"Content"`
+	Replies   []reply `json:"Replies"`
+}
+
+func NewPTT(storePathFolder string) *PTT {
 	p := new(PTT)
 	p.baseUrl = "https://www.ptt.cc/bbs/"
+	p.storePath = storePathFolder
 	p.board = mapset.NewSet()
 	return p
 }
@@ -52,4 +80,87 @@ func isValidBoard(baseurl, board string) bool {
 		return false
 	}
 	return true
+}
+
+func CrawlArticle(url string) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.AddCookie(&http.Cookie{Name: "over18", Value: "1"})
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("CrawlArticle: status code %d %s", resp.StatusCode, resp.Status)
+	}
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return err
+	}
+	author, board, class, title, timestamp := parseArticleAttr(doc.Find(".article-meta-value").Remove())
+	doc.Find(".article-metaline").Remove()
+	doc.Find(".article-metaline-right").Remove()
+
+	// remove the replies if they're in the content
+	content := Split(doc.Find("#main-content").Text(), "\n※ 發信站")[0]
+	doc.Find(".push").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		if Contains(content, s.Text()) {
+			s.Remove()
+		}
+		if i > 4 {
+			return false
+		}
+		return true
+	})
+	replies := make([]reply, 0)
+	doc.Find(".push").Each(func(i int, s *goquery.Selection) {
+		ipdatetime := s.Find(".push-ipdatetime").Text()
+		replies = append(replies, reply{
+			Floor:     i + 1,
+			UserID:    s.Find(".push-userid").Text(),
+			Vote:      s.Find(".push-tag").Text(),
+			Content:   s.Find(".push-content").Text()[2:], // remove : and space
+			Timestamp: ipdatetime[1 : len(ipdatetime)-1],  // remove space and newline
+		})
+		s.Remove()
+	})
+	// now "doc.Find("#main-content").Text()" has all the content including the author's reply in the reply area
+	art := article{Board: board, Class: class, Title: title, Author: author, Timestamp: timestamp, Content: content, Replies: replies}
+	jsondata, err := json.Marshal(art)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Create("out.json")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	f.Write(jsondata)
+	return nil
+}
+
+func parseArticleAttr(s *goquery.Selection) (author, board, class, title, date string) {
+	author = TrimSpace(Split(s.Eq(0).Text(), "(")[0])
+	board = s.Eq(1).Text()
+	class, title = parseTitle(s.Eq(2).Text())
+	date = s.Eq(3).Text()
+	return author, board, class, title, date
+}
+func parseTitle(artTitle string) (string, string) {
+	var class []byte
+	for i := range artTitle {
+		if artTitle[i] == '[' {
+			for artTitle[i] != ']' {
+				class = append(class, artTitle[i])
+				i++
+			}
+			class = append(class, artTitle[i])
+			break
+		}
+	}
+	return TrimSpace(string(class)), TrimSpace(Trim(artTitle, string(class)))
 }
